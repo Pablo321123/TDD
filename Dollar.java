f63a9b16e35106cf996a31ae638ce5b7af77c958
Pablo Vasconcelos <pablo_vasconcelos123@hotmail.com>
2023-09-07 15:28:24 -0300

    Refactor money

--- Dollar.java
+++ Dollar.java
@@ -1,10 +1,17 @@
-class Dollar {
-    int amount = 10;
+class Dollar extends Money {
+    private int amount = 10;
 
     Dollar(int amount) {
+        this.amount = amount;
     }
 
-    void times(int multiplier) {
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
     }
 
 }
